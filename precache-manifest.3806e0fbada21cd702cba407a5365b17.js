60f99753e5511c329899c4baddec82a362824a54
Frederic <lenaerts.frederic@gmail.com>
2019-04-06 11:53:46 +0200

    Updates

---
+++ precache-manifest.3806e0fbada21cd702cba407a5365b17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5355e867ed9b09447159",
+    "url": "/light-dark-theme-switch/static/js/runtime~main.099b2efe.js"
+  },
+  {
+    "revision": "85af36f3ccb69e6492be",
+    "url": "/light-dark-theme-switch/static/js/main.1f4c1cf7.chunk.js"
+  },
+  {
+    "revision": "28ea5fd279cb9735c0e3",
+    "url": "/light-dark-theme-switch/static/js/2.92c92fef.chunk.js"
+  },
+  {
+    "revision": "85af36f3ccb69e6492be",
+    "url": "/light-dark-theme-switch/static/css/main.48632d3c.chunk.css"
+  },
+  {
+    "revision": "b32bfa4b6fdc8bbc7484e94e57fa04b9",
+    "url": "/light-dark-theme-switch/index.html"
+  }
+];
